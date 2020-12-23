106e3508129f2c728e26851df2fd3831c0d24699
shashwatbangar <shashwatbangar@gmail.com>
2020-12-24 00:15:30 +0530

    added styling to sign-in page and added custom button

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import "/Users/shashwatbangar/Desktop/ecommerce/crwn-clothing/src/components/sign-in/sign-in.styles.scss"
+import FormInput from "../form-input/form-input.coponent"
+import CustomButton from "../custom-button/custom-button.component"
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email:'',
+            password:''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email:'', password:''})
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target;
+
+        this.setState({[name]:value})
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+            <form onSubmit={this.handleSubmit}>
+                <FormInput 
+                   handleChange={this.handleChange} 
+                   name="email" 
+                   type="email"
+                   label="email" 
+                   value={this.state.email} 
+                   required />
+                
+                <FormInput 
+                   handleChange={this.handleChange} 
+                   name="password" 
+                   type="password" 
+                   label="password"
+                   value={this.state.password} 
+                   required />
+                
+
+                <CustomButton type="submit"> Sign in </CustomButton>
+            </form>    
+            </div>
+        )
+    }
+
+}
+
+export default SignIn;
